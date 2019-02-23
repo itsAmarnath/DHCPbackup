78f25c98d9ec802f60d2fafa0e99808ee652da94
itsAmarnath <40494037+itsAmarnath@users.noreply.github.com>
2019-02-23 12:20:45 +0000

    Update DHCPbackup.ps1

--- DHCPbackup.ps1
+++ DHCPbackup.ps1
@@ -1,10 +1,5 @@
-﻿$date = ((Get-Date).tostring("dd MMM yyyy"))
-$year = ((Get-Date).tostring("yyyy"))
-$path1= "E:\DHCP Backup\$year\$date"
-$path2= "\\grape\IT\Server\Active Directory\DHCP Backup\DC\$env:COMPUTERNAME\$year\$date\"
+$date = ((Get-Date).tostring("dd MMM yyyy"))
+$path = "D:\Backup\DHCP"
 New-Item -ItemType Directory -Path "$path1" -force
-New-Item -ItemType Directory -Path "$path2" -force
 Backup-DhcpServer -Path $path1
-Start-Sleep -second 20
-copy-item -path "$path1\*" -Destination $path2 -Recurse -Force
-Send-MailMessage -from "DHCP_backup@miramedajuba.com" -to "amarnath.kubendiran@miramedajuba.com" -Subject "DHCP backup completed in $env:COMPUTERNAME " -SmtpServer "exchange.miramedajuba.com"
+
